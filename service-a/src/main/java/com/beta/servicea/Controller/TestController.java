d89cd70a787df08ca84043cfd9015eaf1cbaa31a
bingzhaoshuang <bingzhaoshuang@betainfo.cn>
2019-12-31 09:59:32 +0800

    spring-config

--- service-a/src/main/java/com/beta/servicea/Controller/TestController.java
+++ service-a/src/main/java/com/beta/servicea/Controller/TestController.java
@@ -2,13 +2,17 @@ package com.beta.servicea.Controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
     @Value("${server.port}")
     private String port;
+    @Value("${name}")
+    private String name;
 
     @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping("/hello")
@@ -18,5 +22,11 @@ public class TestController {
 
     String fallback(){
         return "服务器繁忙";
+    }
+
+
+    @RequestMapping("/hello2")
+    String hello2() {
+        return name;
     }
 }
